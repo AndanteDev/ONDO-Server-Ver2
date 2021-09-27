975be5e04101ee37e17459989a3c88e68f898f67
seojinsarejj <s19009@naver.com>
2021-09-27 12:14:13 +0900

    [CREATE] Create diary model

--- app/db/session.py
+++ app/db/session.py
@@ -9,5 +9,6 @@ db_session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind
 Base = declarative_base() 
 Base.query = db_session.query_property() 
 
-def init_db():  
+def init_db(): 
+    from ..models.diary import Diary
     Base.metadata.create_all(engine)
---
+++ app/models/diary.py
@@ -0,0 +1,36 @@
+from datetime import datetime
+from typing import Text
+from sqlalchemy import Column,Integer, Float, Text, DateTime,Date, Enum
+from ..db.session import Base
+import enum
+
+class Emotion(enum.IntEnum):
+    happy = 1
+    sad = 2
+    angry = 3
+    soso = 4
+    tired = 5
+
+class Diary(Base):
+    __tablename__ = "diary"
+
+    diary_id = Column(Integer, primary_key=True, autoincrement=True)
+    user_id = Column(Integer,nullable=False)
+    context = Column(Text)
+    emotion = Column(Enum(Emotion))
+    value = Column(Float)
+    date = Column(Date)
+    created_at = Column(DateTime, nullable=False)
+
+    def __init__(self,user_id,context,emotion,value,date):
+        self.diary_id = None
+        self.user_id = user_id
+        self.context = context
+        self.emotion = emotion
+        self.value = value
+        self.date = date
+        self.created_at = datetime.now()
+        
+
+    def __repr__(self):
+        return "<Diary '{},{}'>".format(self.user_id,self.date)
