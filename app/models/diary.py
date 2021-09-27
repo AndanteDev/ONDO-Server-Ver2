0b3ef155b4a01c161ef9addd7e5e766a76e963a8
seojinsarejj <s19009@naver.com>
2021-09-27 16:59:56 +0900

    [UPDATE] Update sqlalchemy text type

--- app/models/diary.py
+++ app/models/diary.py
@@ -1,8 +1,8 @@
 from datetime import datetime
-from typing import Text
-from sqlalchemy import Column,Integer, Float, Text, DateTime,Date, Enum
+from sqlalchemy import Column, Integer, Float, TEXT, DateTime, Date, Enum
 from ..db.session import Base
 import enum
+
 
 class Emotion(enum.IntEnum):
     happy = 1
@@ -11,18 +11,19 @@ class Emotion(enum.IntEnum):
     soso = 4
     tired = 5
 
+
 class Diary(Base):
     __tablename__ = "diary"
 
     diary_id = Column(Integer, primary_key=True, autoincrement=True)
-    user_id = Column(Integer,nullable=False)
-    context = Column(Text)
+    user_id = Column(Integer, nullable=False)
+    context = Column(TEXT)
     emotion = Column(Enum(Emotion))
     value = Column(Float)
     date = Column(Date)
     created_at = Column(DateTime, nullable=False)
 
-    def __init__(self,user_id,context,emotion,value,date):
+    def __init__(self, user_id, context, emotion, value, date):
         self.diary_id = None
         self.user_id = user_id
         self.context = context
@@ -30,7 +31,6 @@ class Diary(Base):
         self.value = value
         self.date = date
         self.created_at = datetime.now()
-        
 
     def __repr__(self):
-        return "<Diary '{},{}'>".format(self.user_id,self.date)
+        return "<Diary '{},{}'>".format(self.user_id, self.date)
