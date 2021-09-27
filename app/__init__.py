cf10b9a20cbce933ad575f484a4a662d36ae948e
seojinsarejj <s19009@naver.com>
2021-09-27 09:40:26 +0900

    [INIT] Initialize orm

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
+from app.db.mapper import start_mappers
+
+
+
+def init_orm():
+    start_mappers()
+
 
 def create_app() -> FastAPI:
     app = FastAPI()
-
+    init_orm()
     return app
