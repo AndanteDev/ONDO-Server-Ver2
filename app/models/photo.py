e2672d3ebff3cf4d17ff369060c8e6508a77942c
seojinsarejj <s19009@naver.com>
2021-09-28 10:53:06 +0900

    [CREATE] Create photo model

---
+++ app/models/photo.py
@@ -0,0 +1,21 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.sql.schema import ForeignKey
+from ..db.session import Base
+from .diary import Diary
+
+
+class Photo(Base):
+
+    __tablename__ = "photo"
+
+    photo_id = Column(Integer, primary_key=True, autoincrement=True)
+    diary_id = Column(ForeignKey(Diary.diary_id), nullable=False)
+    url = Column(String(256), nullable=False)
+
+    def __init__(self, diary_id, url):
+        self.photo_id = None
+        self.diary_id = diary_id
+        self.url = url
+
+    def __repr__(self):
+        return "<Photo '{}'>".format(self.url)
