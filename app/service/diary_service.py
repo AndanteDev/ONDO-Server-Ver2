34e50912f71c0c7da0cf3116ed599667d469c681
seojinsarejj <s19009@naver.com>
2021-09-30 15:46:52 +0900

    [CREATE] Create get all diaries service

--- app/service/diary_service.py
+++ app/service/diary_service.py
@@ -21,7 +21,33 @@ def save_new_diary(input_dto: DiaryCreateRequestDto) -> DiaryCreateResponseDto:
 
 
 def get_all_diaries(input_dto: DiaryListRequestDto) -> DiaryListResponseDto:
-    pass
+    year = input_dto.year
+    month = input_dto.month
+
+    diary = None
+
+    if year != None and month != None:
+        start = str(year) + "-" + str(month).zfill(2) + "-01"
+        end = str(year) + "-" + str(int(month) + 1).zfill(2) + "-01"
+        diaries = (
+            db_session.query(Diary.diary_id, Diary.date)
+            .filter(Diary.date.between(start, end))
+            .all()
+        )
+    elif year != None and month == None:
+        start = str(year) + "-01-01"
+        end = str(int(year) + 1) + "-01-01"
+        diaries = (
+            db_session.query(Diary.diary_id, Diary.date)
+            .filter(Diary.date.between(start, end))
+            .all()
+        )
+    else:
+        diaries = db_session.query(Diary.diary_id, Diary.date).all()
+
+    diary = [{"diary_id": diary.diary_id, "date": str(diary.date)} for diary in diaries]
+
+    return diary
 
 
 def get_a_diary(
