9c021e4c5d846462de6653793640805f18b7311f
seojinsarejj <s19009@naver.com>
2021-09-30 15:21:08 +0900

    [INIT] Initialize router

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,21 @@
 from fastapi import FastAPI
-from app.db.session import init_db
-
+from .db.session import init_db
 
 
 def init_orm():
     init_db()
 
 
+def init_router(app: FastAPI):
+
+    from .controller.diary_controller import router as diary_router
+
+    for router in [diary_router]:
+        app.include_router(router)
+
+
 def create_app() -> FastAPI:
     app = FastAPI()
     init_orm()
+    init_router(app)
     return app
