a6026056258bb0d9115afc24112106777eced107
seojinsarejj <s19009@naver.com>
2021-10-07 11:23:10 +0900

    [UPDATE] Update user id parameter

--- app/controller/diary_controller.py
+++ app/controller/diary_controller.py
@@ -40,12 +40,13 @@ def create_new_diary(
     request: Request, input_dto: DiaryCreateRequestDto
 ) -> DiaryCreateResponseDto:
 
+    user_id = 1
+
     input_dto = DiaryCreateRequestDto(
-        user_id=1,
         context=input_dto.context,
         emotion=input_dto.emotion,
         value=input_dto.value,
         date=input_dto.date,
         photos=input_dto.photos,
     )
-    return save_new_diary(input_dto)
+    return save_new_diary(user_id, input_dto)
