abe6a8cd9f4d6cf7c124ff2ce6bb77a4ae641358
seojinsarejj <s19009@naver.com>
2021-10-07 11:13:37 +0900

    [CREATE] Create save new diary service

--- app/service/diary_service.py
+++ app/service/diary_service.py
@@ -14,17 +14,61 @@ from ..dto.diary import (
     DiaryUpdateRequestDto,
     DiaryUpdateResponseDto,
 )
+from datetime import datetime
+
+
+def save_changes(data):
+    try:
+        db_session.add(data)
+        db_session.commit()
+    except Exception as e:
+        print(e)
 
 
 def save_new_diary(input_dto: DiaryCreateRequestDto) -> DiaryCreateResponseDto:
-    pass
+
+    today_diary = (
+        db_session.query(Diary).filter(Diary.created_at == datetime.now()).first()
+    )
+
+    if not today_diary:
+
+        new_diary = Diary(
+            input_dto.user_id,
+            input_dto.context,
+            input_dto.emotion,
+            input_dto.value,
+            input_dto.date,
+        )
+
+        save_changes(new_diary)
+
+    else:
+        pass
+
+    created_photos = []
+    for photo in input_dto.photos:
+        created_photos.append(photo)
+
+    response = {
+        "diary_id": new_diary.diary_id,
+        "user_id": new_diary.user_id,
+        "context": new_diary.context,
+        "emotion": new_diary.emotion,
+        "value": new_diary.value,
+        "date": str(new_diary.date),
+        "created_at": str(new_diary.created_at),
+        "photos": created_photos,
+    }
+
+    return response
 
 
 def get_all_diaries(input_dto: DiaryListRequestDto) -> DiaryListResponseDto:
     year = input_dto.year
     month = input_dto.month
 
-    diary = None
+    diary = []
 
     if year != None and month != None:
         start = str(year) + "-" + str(month).zfill(2) + "-01"
@@ -72,11 +116,3 @@ def count_diary(user_id: int) -> DiaryCountResponseDto:
 
 def diary_card_list(user_id: int) -> DiaryCardListResponseDto:
     pass
-
-
-def save_changes(data):
-    try:
-        db_session.add(data)
-        db_session.commit()
-    except Exception as e:
-        print(e)
