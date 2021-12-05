a4ba7b6ae7a112b206cbbdc18d8d437df7764dfc
seojinsarejj <s19009@naver.com>
2021-12-05 16:13:54 +0900

    [ADD] Add error handler on s3 util

--- app/utils/util.py
+++ app/utils/util.py
@@ -1,4 +1,5 @@
 import boto3
+from fastapi.exceptions import HTTPException
 from ..config import CONFIG
 
 
@@ -21,18 +22,20 @@ class Util:
 
             url = "https://s3-%s.amazonaws.com/%s/%s" % (region, bucket_name, filename)
 
-        except Exception as e:
-            return e
+        except:
+            raise HTTPException(status_code=500, detail="Fail to upload photo to s3")
 
         return url
 
     def s3delete(filename):
+        try:
+            s3 = boto3.client(
+                "s3",
+                aws_access_key_id=CONFIG.AccessKeyId,
+                aws_secret_access_key=CONFIG.SecretKey,
+            )
 
-        s3 = boto3.client(
-            "s3",
-            aws_access_key_id=CONFIG.AccessKeyId,
-            aws_secret_access_key=CONFIG.SecretKey,
-        )
-
-        bucket_name = CONFIG.bucket_name
-        s3.delete_object(Bucket=bucket_name, Key=filename)
+            bucket_name = CONFIG.bucket_name
+            s3.delete_object(Bucket=bucket_name, Key=filename)
+        except:
+            raise HTTPException(status_code=500, detail="Fail to delete photo on s3")
